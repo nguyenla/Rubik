9788a310812abafbe56713c38cd81f72a7d28082
phulamnjc@gmail.com <phulamnjc@gmail.com>
2016-11-15 17:01:28 -0500

    Set up skeleton of the RubikCube class

--- src/RubikCube.java
+++ src/RubikCube.java
@@ -1,7 +1,72 @@
 
 public class RubikCube {
+	private int[] up;
+	private int[] down;
+	private int[] left;
+	private int[] right;
+	private int[] front;
+	private int[] back;
 
+	public RubikCube(String orientation) {
+
+	}
+
+	// Rotate up
+	public void rotateUpClockwise() {
+
+	}
+
+	public void rotateUpAntiClockwise() {
+
+	}
+	
+	// Rotate down
+	public void rotateDownClockwise() {
+
+	}
+
+	public void rotateDownAntiClockwise() {
+
+	}
+	
+	// Rotate left
+	public void rotateLeftClockwise() {
+
+	}
+
+	public void rotateLeftAntiClockwise() {
+
+	}
+	
+	// Rotate right
+	public void rotateRightClockwise() {
+
+	}
+
+	public void rotateRightAntiClockwise() {
+
+	}
+	
+	// Rotate front
+	public void rotateFrontClockwise() {
+
+	}
+
+	public void rotateFrontAntiClockwise() {
+
+	}
+	
+	// Rotate back
+	public void rotateBackClockwise() {
+
+	}
+
+	public void rotateBackAntiClockwise() {
+
+	}
+
+	// Main method to run driver tests
 	public static void main(String[] args) {
-		
+
 	}
 }
